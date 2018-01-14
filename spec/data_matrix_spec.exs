49228e35183ee54c7b62abb4944b89d391a49c96
Dunya Kirkali <dunyakirkali@gmail.com>
2018-01-14 20:49:25 +0100

    Add sample specs

---
+++ spec/data_matrix_spec.exs
@@ -0,0 +1,19 @@
+defmodule DataMatrixSpec do
+  doctest DataMatrix
+
+  let :tolerance, do: {:minutes, 3}
+
+  describe "Amsterdam" do
+
+    describe "on Jan 14th 2018" do
+
+      it "rise" do
+
+      end
+
+      it "set" do
+
+      end
+    end
+  end
+end
